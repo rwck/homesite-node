e42a6f93cb2f81a5a9d276389a4abeff908d26c0
Donald Wade <rwckdw@gmail.com>
2016-01-08 00:54:30 +1100

    hopefully got the mongodb side of things working now

--- broken/app.js
+++ broken/app.js
@@ -1,11 +1,11 @@
 var express = require('express'),
-  app = express();
+  app = express(),
   engines = require('consolidate');
-bodyParser = require('body-parser');
+  bodyParser = require('body-parser');
+
 
 app.engine('html', engines.nunjucks);
 app.set('view engine', 'html');
-app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/views');
 app.use(bodyParser.urlencoded({
   extended: true
@@ -23,26 +23,28 @@ function errorHandler(err, req, res, next) {
 var MongoClient = require('mongodb').MongoClient,
   assert = require('assert');
 
-MongoClient.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/challenge1", function(err, db) {
+
+// home: mongodb://localhost:27017/homesite
+
+// heroku: mongodb://<dbuser>:<dbpassword>@ds039155.mongolab.com:39155/heroku_vcdcg3t9
+
+MongoClient.connect(MONGOLAB_URI, function(err, db) {
   assert.equal(null, err, function() {
-    console.log("There has been a problem connecting to the database");
+    console.log("There has been an error with the database", err.message);
   });
+  console.log("Successfully connected to MongoDB");
 
   app.get('/:var(|home)?', function(req, res, next) {
-    var links = ["Portfolio", "Bio", "Contact"];
-    res.render('index', {
-      links: links
-    });
+    var links = [ "Portfolio", "Bio", "Contact" ];
+    res.render('index', { links: links });
   });
 
   app.get('/portfolio', function(req, res, next) {
-    var links = ["Home", "Bio", "Contact"];
-    res.render('portfolio', {
-      links: links
-    });
+    var links = [ "Home", "Bio", "Contact" ];
+    res.render('portfolio', { links: links });
   });
 
-  var server = app.listen(app.get('port'), function() {
+  var server = app.listen(3333, function() {
     var port = server.address().port;
     console.log("Express is setup and listening on %s", port);
   });
